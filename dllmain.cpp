dd8ad6a04d86bca8f35390575fad16df1ca229c5
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-02-28 20:07:31 +0100

    Split EAT hook in different functions

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,6 +1,6 @@
 //      EAT Redirection - The EAT, or Export Address Table is similar to the
 //		IAT.  Except in the opposite direction.  When a module exports a function 
-//		so that it can be used by other modules, it stores the address of  
+//		so that it can be used by other modules, it stores the address of
 //		that function in it's EAT.  EAT redirection overwrites that address 
 //		with the offset of your hook.  EAT redirection will not affect any 
 //		currently loaded modules.  It will only affect modules loaded after the 
@@ -20,7 +20,7 @@ Console console;
 ----------------------------------------------------*/
 typedef int(WINAPI* TrueMessageBox)(HWND, LPCTSTR, LPCTSTR, UINT);
 
-TrueMessageBox trueMessageBox = NULL;
+TrueMessageBox trueMessageBox = MessageBoxW;
 
 BOOL WINAPI MessageBoxHook(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType)
 {
@@ -28,91 +28,147 @@ BOOL WINAPI MessageBoxHook(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uT
 	return trueMessageBox(hWnd, lpText, lpCaptionChanged, uType);
 }
 
-#include <fstream>
-PDWORD get_export_offset_address(uintptr_t module_base_address, const char* function_name)
-{
-
-	// Parse PE header
-	PIMAGE_DOS_HEADER dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(module_base_address);
-	PIMAGE_NT_HEADERS ntHeader = reinterpret_cast<PIMAGE_NT_HEADERS>(module_base_address + dosHeader->e_lfanew);
+// Parse the PE header to find the address of the Export Directory Table
+PIMAGE_EXPORT_DIRECTORY get_export_directory(DWORD base)
+{	
+	PIMAGE_DOS_HEADER dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(base);
+	PIMAGE_NT_HEADERS ntHeader = reinterpret_cast<PIMAGE_NT_HEADERS>(base + dosHeader->e_lfanew);
 	IMAGE_DATA_DIRECTORY export_datadir = ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
-	PIMAGE_EXPORT_DIRECTORY export_directory = reinterpret_cast<PIMAGE_EXPORT_DIRECTORY>(module_base_address + export_datadir.VirtualAddress);
+	PIMAGE_EXPORT_DIRECTORY export_directory = reinterpret_cast<PIMAGE_EXPORT_DIRECTORY>(base + export_datadir.VirtualAddress);
 
+	return export_directory;
+}
+
+// Find the correct function in the Export Name Pointer Table
+DWORD get_name_pointer_table_index(DWORD base, PIMAGE_EXPORT_DIRECTORY export_directory, const char* function_name)
+{
 	// Get addresses of the arrays
-	PDWORD name_offset_array = reinterpret_cast<PDWORD>(module_base_address + export_directory->AddressOfNames);
-	PWORD ordinal_array = reinterpret_cast<PWORD>(module_base_address + export_directory->AddressOfNameOrdinals); //word because ordinal table has 16 bit entries
-	PDWORD function_offset_array = reinterpret_cast<PDWORD>(module_base_address + export_directory->AddressOfFunctions);
+	PDWORD name_offset_array = reinterpret_cast<PDWORD>(base + export_directory->AddressOfNames);
 
-	// Cycle through all function pointers
-	for (int i=0; i < export_directory->NumberOfFunctions; ++i)
+	// Cycle through all pointers in the Export Name Pointer Table 
+	// (contains pointers to the ASCII names of the functions)
+	for (DWORD i = 0; i < export_directory->NumberOfFunctions; ++i)
 	{
-		const char* current_name = reinterpret_cast<const char*>(module_base_address + name_offset_array[i]);
-
-		fprintf(console.stream, "%s \n", current_name);
-
-		if (_stricmp(function_name, current_name))
-			continue;
-
-		PWORD ordinal_base = reinterpret_cast<PWORD>(module_base_address + export_directory->Base);
-		WORD indexEAT = ordinal_array[i] - *ordinal_base; //biased ordinal? => have to subtract ordinal base?
-
-		fprintf(console.stream, "%d \n", indexEAT);
-		fprintf(console.stream, "%d \n", *ordinal_base);
-
-		// We want to get the address of the DWORD, so we use pointer arithmetics instead of function_offset_array[current_ordinal]
-		PDWORD current_function_offset = function_offset_array + indexEAT;
-
-		return current_function_offset;
+		// Follow the pointer to the ASCII names of the functions in the Export Name Table
+		const char* current_name = reinterpret_cast<const char*>(base + name_offset_array[i]);
+		
+		if (_stricmp(function_name, current_name) == 0) {
+			fprintf(console.stream, "Found module %s in Export Name Table at index %d\n", function_name, i);
+			return i;
+		}
 	}
 
-	return nullptr;
+	return -1;
 }
 
-
-DWORD WINAPI installEATHook(PVOID base) {
-
-	TrueMessageBox hookedMessageBox1 = reinterpret_cast<TrueMessageBox>(GetProcAddress(GetModuleHandle(L"user32.dll"), "MessageBoxW"));
-	hookedMessageBox1(NULL, L"before hook with GetProcAddress", L"box with getProcAddress", MB_OK);
+// Use the index in the Export Ordinal Table (forms a parallel array with the Export Name Pointer Table
+// Add the Export Address Table RVA to find the address of the exported function
+PDWORD get_export_offset_address(DWORD base, PIMAGE_EXPORT_DIRECTORY export_directory, DWORD index)
+{
+	PWORD ordinal_array = reinterpret_cast<PWORD>(base + export_directory->AddressOfNameOrdinals); //word because ordinal table has 16 bit entries
 	
-	uintptr_t module_base_address = (uintptr_t)GetModuleHandle(L"user32.dll");
-	PDWORD messageBoxW_offset_address = get_export_offset_address(module_base_address, "MessageBoxW");
+	PWORD ordinal_base = reinterpret_cast<PWORD>(base + export_directory->Base);
 
-	if (messageBoxW_offset_address == nullptr) {
-		return FALSE;
-	}
+	//biased ordinal? => have to subtract ordinal base?
+	WORD indexEAT = ordinal_array[index] - *ordinal_base; 
 
-	DWORD hook_offset = (uintptr_t)&MessageBoxHook - module_base_address;
+	PDWORD function_offset_array = reinterpret_cast<PDWORD>(base + export_directory->AddressOfFunctions);
 
-	DWORD old_protection{};
-	VirtualProtect(messageBoxW_offset_address, sizeof(DWORD), PAGE_READWRITE, &old_protection);
-	*messageBoxW_offset_address = hook_offset;
-	VirtualProtect(messageBoxW_offset_address, sizeof(DWORD), old_protection, &old_protection);
+	// We want to get the address of the DWORD, so we use pointer arithmetics instead of function_offset_array[current_ordinal]
+	PDWORD current_function_offset = function_offset_array + indexEAT;
 
-	// open messagebox for test
-	TrueMessageBox hookedMessageBox2 = reinterpret_cast<TrueMessageBox>(GetProcAddress(GetModuleHandle(L"user32.dll"), "MessageBoxW"));
-	hookedMessageBox2(NULL, L"after hook with GetProcAddress", L"box with getProcAddress", MB_OK);
+	// Note: the Export Address Table can also contain a forwarder, 
+	// in this case the address will point to outside the current module
 
-	//Loadlibrary again to affect program that doesnt use GetProcAddress?
-
-	return TRUE;
+	return current_function_offset;
 }
+
+// Install the hook (overwrite the pointer in the Export Address Table)
+void overwriteEAT(DWORD base, PDWORD offset, DWORD pointer) {
+	// Change the protection so we can overwrite the pointer, store the old protection
+	DWORD old_protection{};
+	VirtualProtect(offset, sizeof(DWORD), PAGE_READWRITE, &old_protection);
+
+	// Overwrite the address with a pointer to another function
+	*offset = pointer;
+
+	// Restore the old protection
+	VirtualProtect(offset, sizeof(DWORD), old_protection, &old_protection);
+}
+
+LPCSTR module_name = "user32.dll";
+LPCSTR function_name = "MessageBoxW";
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
-    {
+	switch (ul_reason_for_call)
+	{
 	case DLL_PROCESS_ATTACH: {
 		if (!console.open()) {
 			// Indicate DLL loading failed
 			return FALSE;
 		}
-		CreateThread(nullptr, NULL, installEATHook, hModule, NULL, nullptr); break;
+
+		// module handle == base address of the module
+		HMODULE h_module = GetModuleHandleA(module_name);
+		if (h_module == NULL) {
+			fprintf(console.stream, "Unable to get handle for module %s \n", module_name);
+			return TRUE;
+		}
+		fprintf(console.stream, "Found handle %p for module %s \n", h_module, module_name);
+
+		// module handle == base address of the module
+		// but we need to cast it to do correct pointer arithmetic
+		DWORD base = (DWORD)h_module;
+
+		PIMAGE_EXPORT_DIRECTORY p_export_dir = get_export_directory(base);
+		fprintf(console.stream, "Found address %p for Export Directory Table of module %s\n", p_export_dir, module_name);
+
+		DWORD index = get_name_pointer_table_index(base, p_export_dir, function_name);
+		if (index == -1) {
+			fprintf(console.stream, "Unable to find function %s in Export Name Pointer Table\n", function_name);
+			return TRUE;
+		}
+		fprintf(console.stream, "Found function %s at index %d in Export Name Pointer Table\n", function_name, index);
+
+
+
+		PDWORD offset_address = get_export_offset_address(base, p_export_dir, index);
+		fprintf(console.stream, "Found address %d of exported function %s in the Export Address Table\n", base+*offset_address, function_name);// EAT contains an offset from the base
+		fprintf(console.stream, "True function is located at address %p\n", trueMessageBox);		
+
+		DWORD hook_offset = (DWORD)&MessageBoxHook - base;
+		overwriteEAT(base, offset_address, hook_offset);
+		fprintf(console.stream, "Overwrote the pointer in the Export Address Table with the pointer to our original function %d\n", hook_offset);
+
+		// todo: test that install was successfull by using GetProcAddress
+		FARPROC address = GetProcAddress(h_module, function_name);
+		fprintf(console.stream, "Hook function is located at address %p\n", &MessageBoxHook);
+		fprintf(console.stream, "GetProcAddress found address %p for function %s\n", address, function_name);
+		
+
+		if (address != nullptr) {
+			// use the address to open a messagebox to prove it points to our hook
+			TrueMessageBox hookedMessageBox2 = reinterpret_cast<TrueMessageBox>(address);
+			hookedMessageBox2(NULL, L"EAT hook test", L"EAT hook test", MB_OK);
+
+		}
+
+		//Loadlibrary again to affect program that doesnt use GetProcAddress?
+		// EAT will only affect modules loaded after the redirection	has been made.
+
+		return TRUE;
 	}
-        
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+
+	case DLL_THREAD_ATTACH: {}
+	case DLL_THREAD_DETACH: {}
+	case DLL_PROCESS_DETACH: {
+		// todo: overwrite the address with the original address 
+		//overwriteEAT(base, offset_address, (DWORD)trueMessageBox);
+
+		// todo: test that install was successfull by using GetProcAddress
+		// call test
+		return TRUE;
+	}
+	}
 }
