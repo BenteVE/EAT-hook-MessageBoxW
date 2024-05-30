a3ae9db3fc2be540f7fd65734626ac2ed4b8f1da
Bente Van Eeckhoudt <bente.van.eeckhoudt@hotmail.com>
2024-05-30 20:23:20 +0200

    VS code formatting

--- dllmain.cpp
+++ dllmain.cpp
@@ -11,7 +11,7 @@ Console console;
 LPCSTR module_name = "user32.dll";
 LPCSTR function_name = "MessageBoxW";
 
-typedef int(WINAPI* TrueMessageBox)(HWND, LPCWSTR, LPCWSTR, UINT);
+typedef int(WINAPI *TrueMessageBox)(HWND, LPCWSTR, LPCWSTR, UINT);
 
 TrueMessageBox trueMessageBox = MessageBoxW;
 
@@ -23,7 +23,7 @@ BOOL WINAPI MessageBoxHook(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uT
 
 // Parse the PE header to find the address of the Export Directory Table
 PIMAGE_EXPORT_DIRECTORY get_export_directory(UINT_PTR base)
-{	
+{
 	PIMAGE_DOS_HEADER dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(base);
 	PIMAGE_NT_HEADERS ntHeader = reinterpret_cast<PIMAGE_NT_HEADERS>(base + dosHeader->e_lfanew);
 	IMAGE_DATA_DIRECTORY export_datadir = ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
@@ -32,24 +32,25 @@ PIMAGE_EXPORT_DIRECTORY get_export_directory(UINT_PTR base)
 	return export_directory;
 }
 
-// the Export Name Pointer Table contains pointers to ASCII strings in the Export Name Table 
+// the Export Name Pointer Table contains pointers to ASCII strings in the Export Name Table
 // that contains the names of all functions that are exported by the module
 // - we iterate all pointers and compare the names until we find the correct function
 // - we then return the index of that function
-DWORD get_function_index(UINT_PTR base, PIMAGE_EXPORT_DIRECTORY export_directory, const char* function_name)
+DWORD get_function_index(UINT_PTR base, PIMAGE_EXPORT_DIRECTORY export_directory, const char *function_name)
 {
 	// Get a pointer to the start of the Export Name Pointer Table
 	// Note: the pointers are relative to the module base address
 	PDWORD export_name_pointer_table = reinterpret_cast<PDWORD>(base + export_directory->AddressOfNames);
 
-	// Cycle through all pointers in the Export Name Pointer Table 
+	// Cycle through all pointers in the Export Name Pointer Table
 	// (contains pointers to the ASCII names of the functions)
 	for (DWORD i = 0; i < export_directory->NumberOfFunctions; ++i)
 	{
 		// Follow the pointer to the ASCII names of the functions in the Export Name Table
-		const char* current_name = reinterpret_cast<const char*>(base + export_name_pointer_table[i]);
-		
-		if (_stricmp(function_name, current_name) == 0) {
+		const char *current_name = reinterpret_cast<const char *>(base + export_name_pointer_table[i]);
+
+		if (_stricmp(function_name, current_name) == 0)
+		{
 			fprintf(console.stream, "Found %s in Export Name Table at index %d\n", function_name, i);
 			return i;
 		}
@@ -60,7 +61,8 @@ DWORD get_function_index(UINT_PTR base, PIMAGE_EXPORT_DIRECTORY export_directory
 }
 
 // Install the hook (overwrite the pointer in the Export Address Table)
-void overwriteEAT(PDWORD hook_address, DWORD function_address) {
+void overwriteEAT(PDWORD hook_address, DWORD function_address)
+{
 	// Change the protection so we can overwrite the pointer, store the old protection
 	DWORD old_protection{};
 	VirtualProtect(hook_address, sizeof(DWORD), PAGE_READWRITE, &old_protection);
@@ -77,35 +79,40 @@ void overwriteEAT(PDWORD hook_address, DWORD function_address) {
 
 // Storing these values to be able to use them in the attach and detach
 HMODULE h_module = NULL;
-UINT_PTR base = 0; //Using UINT_PTR because it scales to the size of a pointer for both 32-bit and 64-bit Windows 
+UINT_PTR base = 0;			   // Using UINT_PTR because it scales to the size of a pointer for both 32-bit and 64-bit Windows
 PDWORD hook_address = nullptr; // The entries in the Export Address Table are 4 bytes long
 
 // Testing the hook by retrieving the address of the function with GetProcAddress
-void test_hook() {
+void test_hook()
+{
 	FARPROC address = GetProcAddress(h_module, function_name);
 	fprintf(console.stream, "\nTesting with GetProcAddress: %p\n", address);
 
 	// Using the address retrieved by GetProcAddress to create a MessageBox
 	// (If the hook is active, the text will be changed)
-	if (address != nullptr) {
+	if (address != nullptr)
+	{
 		TrueMessageBox messageBox = reinterpret_cast<TrueMessageBox>(address);
 		messageBox(NULL, L"Regular text", L"Regular caption", MB_OK);
 	}
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH: {
-		if (!console.open()) {
+	case DLL_PROCESS_ATTACH:
+	{
+		if (!console.open())
+		{
 			// Indicate DLL loading failed
 			return FALSE;
 		}
 
 		// module handle == base address of the module
 		h_module = GetModuleHandleA(module_name);
-		if (h_module == NULL) {
+		if (h_module == NULL)
+		{
 			fprintf(console.stream, "Unable to get handle for module %s \n", module_name);
 			return TRUE;
 		}
@@ -122,12 +129,13 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 		// In this example, we identify the function we want to hook by the function name (not by an ordinal)
 		// => we first have to use the following tables to obtain the ordinal of the function
 		// - the Export Name Pointer Table
-		// - the Export Name Table 
+		// - the Export Name Table
 		// - the Export Ordinal Table
 
 		// Iterate the Export Name Pointer Table and return the index when we find the function name
 		DWORD index = get_function_index(base, p_export_dir, function_name);
-		if (index == -1) {
+		if (index == -1)
+		{
 			return TRUE;
 		}
 
@@ -137,21 +145,19 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 		WORD biased_ordinal = export_ordinal_table[index];
 		fprintf(console.stream, "Biased ordinal: %d\n", biased_ordinal);
 
-		// Ordinals are biased by the Ordinal Base field of the export directory table. 
+		// Ordinals are biased by the Ordinal Base field of the export directory table.
 		// In other words, the ordinal base must be subtracted from the ordinals to obtain true indexes into the export address table
 		// Note: if we wanted to hook a function identified by an ordinal, we could do this step immediately
 		DWORD ordinal_base = *reinterpret_cast<PDWORD>(base + p_export_dir->Base);
 		DWORD unbiased_ordinal = biased_ordinal - ordinal_base;
 		fprintf(console.stream, "Ordinal base: %d\n", ordinal_base);
 		fprintf(console.stream, "Unbiased ordinal: %d\n", unbiased_ordinal);
-		
 
 		// The unbiased ordinal can be used as an index into the Export Address Table
-		// => we overwrite the value we find with the 
+		// => we overwrite the value we find with the
 		PDWORD export_address_table = reinterpret_cast<PDWORD>(base + p_export_dir->AddressOfFunctions);
 		hook_address = export_address_table + unbiased_ordinal;
 
-		
 		// The Export Address Table contains 4 byte offsets to the exported functions
 		// the offsets are relative to the base address of the module
 		// => we need to calculate the offset to our hook function
@@ -160,12 +166,12 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 		// => the calculation of the offset then causes an underflow
 		// => we need to also cause an overflow when the base gets added to the offset
 		// This is only possible in 32 bit because the offset and base have the same bit size
-		if (base + hook_function_offset != (UINT_PTR)&MessageBoxHook) {
+		if (base + hook_function_offset != (UINT_PTR)&MessageBoxHook)
+		{
 			fprintf(console.stream, "Problem with under/overflow for offset.\n");
 			fprintf(console.stream, "Setting pointer to NULL instead, this blocks function execution.\n");
 			hook_function_offset = 0;
 		}
-
 
 		fprintf(console.stream, "\n");
 		fprintf(console.stream, "True fn:   %p\n", (UINT_PTR)trueMessageBox);
@@ -176,7 +182,7 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 		// Overwrite the value
 		overwriteEAT(hook_address, hook_function_offset);
 
-		// Note: when a module is loaded in the memory, 
+		// Note: when a module is loaded in the memory,
 		// - the symbols from the EAT of the module will be loaded in the IAT of the process
 		// - the process will then check the IAT whenever it needs access to that symbol
 		// This means the EAT hook will have no effect when the process uses implicit linking
@@ -185,19 +191,20 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 
 		return TRUE;
 	}
-						   
 
-	case DLL_THREAD_ATTACH: break;
-	case DLL_THREAD_DETACH: break;
-	case DLL_PROCESS_DETACH: {
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+	{
 		// overwrite the address with the original address (unhook)
 		fprintf(console.stream, "\nUnhooking...\n");
 		DWORD true_function_offset = (DWORD)((UINT_PTR)trueMessageBox - base);
 		overwriteEAT(hook_address, true_function_offset);
 		test_hook();
-		
+
 		return TRUE;
 	}
-
 	}
 }
